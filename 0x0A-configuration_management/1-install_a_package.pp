51b821034d2a2a3af40503877be2e73d7f836631
ridabayi <bayi.rida@gmail.com>
2024-07-03 19:02:44 -0700

    installing packages & execut

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,6 @@
 # Puppet manifest to install Flask package from pip3
-
-package { 'Flask':
-  ensure => '2.1.0',
+# puppet declarative script to install flask from pip3.
+package { 'flask':
+  ensure   => '2.1.0',
   provider => 'pip3',
-  require => Exec['update-pip3'],
-}
-
-exec { 'update-pip3':
-  command => 'pip3 install --upgrade pip',
-  path    => '/usr/local/bin',
-  creates => '/usr/local/bin/pip3',
 }
