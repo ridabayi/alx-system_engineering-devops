c64081e898ce3bc0f4dd44a20342267b82d23b45
ridabayi <bayi.rida@gmail.com>
2024-08-31 04:56:55 -0700

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
 # Using puppet to install flask from pip3
+
+package { 'python3.8':
+        ensure  => '3.8.10',
+        provider => 'pip3',
+}
+
 package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
+}
+
+package { 'werkzeug':
+    ensure  => '2.1.1',
+    provider  => 'pip3',
+    require   =>  Package['Flask'],
 }
